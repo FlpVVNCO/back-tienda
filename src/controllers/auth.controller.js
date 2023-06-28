849fe5c7cf638ac1420113743d8208f7364a3d3e
Felipe Vivanco <felipe.vasquez@coredevx.com>
2023-06-28 11:08:40 -0400

    Creación jwt

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,25 +1,37 @@
 import User from "../models/user.model.js"
+import bcrypt from 'bcryptjs'
+import { createAccessToken } from "../libs/jwt.js";
 
 export const register = async (req, res) => {
    
     const {username, email, password} = req.body
 
-    console.log(username, email, password);
-
     try {
+
+        const passwordHash = await bcrypt.hash(password, 10)
+
         const newUser = new User({
             username,
             email,
-            password
+            password: passwordHash,
         })
 
         const userSaved = await newUser.save();
-        res.json(userSaved)
+        const token = await createAccessToken({id: userSaved._id});
+        res.cookie('token', token)
+        res.json({
+            id: userSaved._id,
+            username: userSaved.username,
+            email: userSaved.email,
+            createdAt: userSaved.createdAt,
+            updatedAt: userSaved.updatedAt,
+        });
+        res.send('registrando')
     } catch (error) {
-        console.log(error)
+        res.status(500).json({message: error.message})
     }
 
-   res.send('registrando')
+
 };
 
 export const login = (req, res) => res.send("login");
