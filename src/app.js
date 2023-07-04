af6bf176d07127f22bb021b62779c5613e3d4435
Felipe Vivanco <felipe.vasquez@coredevx.com>
2023-07-04 15:19:32 -0400

    Integracion de cors/ manejo de errores cambiados a array

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser"
+import cors from 'cors'
 
 import authRoutes from './routes/auth.routes.js'
 import taskRoutes from './routes/tasks.route.js'
 
 const app = express();
 
+// averiguar como manejar los cors
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(cookieParser())
